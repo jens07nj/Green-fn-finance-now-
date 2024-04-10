b2b96a38044ecfdbfafdc6703b4f223bd2a1005e
jens07nj <jakejensen07@icloud.com>
2024-04-10 12:11:53 +1000

    test comment
    I have completed the psuedocode for my app green fn and the functions with in it - money buckets and compound interest.
    
    I began converting my pseudocode into python.
    
    today I continued and finnished coverting my pseudocode. i then tested my code and encountered problems regarding the maths side of my compound interest function but resuved them. i then just went over my code and make sure the app was easy for the user to interprate and use.
    
    added foot notes to make code easier to comprehend and be understood by someone looking at the code.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def moneybuckets():
+    #welcome to money buckets, explain game
     print('welcome to money buckets where we will plan how you can live on 60% of your income and grow your wealth ')
-    income =int( input('What is your income?') )
+    #user input income
+    income =float( input('What is your income?') )
     print('your money buckets')
+    #divie income and print in diveded categorys
     blow = income * 60 / 100
     print(f'blow money: ${income *60/100}')
     print(f'   ${blow*60/100} of your blow money is for daily expences')
@@ -13,10 +16,13 @@ def moneybuckets():
 
     return
 def compoundinterest():
-    age = int(input ("What age are you begining to invest?"))
-    interest= int(input ('average annual interest return (e.g. 10%):'))
-    add = int(input ('How much will you invest every year:'))
+    #user input age, interest rate and how much money they add each year.
+    age = int(input ("What age are you beginning to invest?"))
+    interest= float(input ('average annual interest return (e.g. 10%):'))
+    add = float(input ('How much will you invest every year:'))
+    # print headings of table
     print ('age   invested   savings')
+    #print contents of table looped till age is 60
     bal = 0
     while age < 61:
         bal = bal + add + (bal + add)* interest/100
@@ -25,9 +31,10 @@ def compoundinterest():
 
 
     return
-
+#welcome to app
 print('welcome to green fn')
 game =(input ('to select money buckets enter (1), to select compound interest enter (2), to end game enter (3):'))
+#user chooses a game or to end app, run chosen game
 while game != '':
     if game =='1':
         moneybuckets()
